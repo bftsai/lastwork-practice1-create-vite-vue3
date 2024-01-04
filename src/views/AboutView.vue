e6d35a07ccfb989a27946626ef034febea1af016
bf_tsai <tsai19911026@gmail.com>
2024-01-05 02:14:29 +0800

    docs: ✏️ add bootstrap axios hexschoolAPI

---
+++ src/views/AboutView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="about">
+    <div class="container">
+      <h1>This is an about page</h1>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return {
+
+    }
+  },
+  created(){
+    console.log(import.meta.env.VITE_HEX_APIKEY);
+    console.log(import.meta.env.VITE_HEX_API);
+  }
+}
+</script>
+<style lang="scss">
+
+</style>
